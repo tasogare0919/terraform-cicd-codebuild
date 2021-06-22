d75b3b41d04f519632a9dbf5b7e59113742fef10
Sadayoshi Tada <tadainfra@gmail.com>
2021-06-22 12:47:07 +0900

    スクリプト移動に伴い削除

--- scripts/apply.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-terrafrom init -input=false -no-color
-terrafrom apply -input=false -no-color -auto-approve
--- scripts/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-BASE_URL=https://github.com/mercari/tfnotify/release/download
-
-DOWNLOAD_URL="${BASE_URL}/v0.6.1/tfnotify_linux_amd64.tar.gz"
-
-wget ${DOWNLOAD_URL} -P /tmp
-tar zxvf /tmp/tfnotify_linux_amd64.tar.gza -C /tmp
-mv /tmp/tfnotify_linux_amd64/tfnotify /usr/local/bin/tfnotify
